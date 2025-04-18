169149c2a609192024d0cf86fad26ad523f1cf57
bhojanipriyanshu <bhojanipriyanshu@gmail.com>
2025-04-18 14:03:46 +0530

    first commit

---
+++ src/Components/Header/Links.tsx
@@ -0,0 +1,20 @@
+import type { SelectedPage } from "@/Components/Shared/Types"
+import Link from "./Link"
+import { links } from "@/Components/Shared/Consts"
+
+type Props = {
+  selectedPage: SelectedPage
+  setSelectedPage: (value: SelectedPage) => void
+}
+
+const Links = ({ selectedPage, setSelectedPage }: Props) => {
+  return (
+    <>
+      {links.map((link) => (
+        <Link key={link} page={link} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      ))}
+    </>
+  )
+}
+
+export default Links
---
+++ src/Components/Main/About/about.tsx
@@ -0,0 +1,11 @@
+import AboutHYGO from "@/Components/Main/About/AboutHYGO"
+
+const About = () => {
+  return (
+    <div className="about-page">
+      <AboutHYGO />
+    </div>
+  )
+}
+
+export default About
